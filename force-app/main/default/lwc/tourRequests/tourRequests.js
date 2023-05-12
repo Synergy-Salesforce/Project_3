d24ba67bd3e862f5b72b87189201dbbdc3116ef9
Taylor B <tinacooltu@gmail.com>
2023-05-12 14:09:25 -0700

    New commit

--- force-app/main/default/lwc/tourRequests/tourRequests.js
+++ force-app/main/default/lwc/tourRequests/tourRequests.js
@@ -6,16 +6,23 @@ import EMAIL from '@salesforce/schema/Tour_Request__c.Email__c';
 import PHONE from '@salesforce/schema/Tour_Request__c.Phone__c';
 import NAME from '@salesforce/schema/Tour_Request__c.Name__c';
 export default class TourRequests extends LightningElement {
-
+/*
     objectApiName = "Tour_Request__c";
     fields = [NAME, EMAIL, PHONE, PROPERTY, AVAILABLE_DATE];
+*/
+@track showMsg = false;
+autoCloseTime = 5000; 
 
+    showMsg = false;
+    @track toastMsg;
+    @api recordId
     NAME = "Name__c";
-    PHONE = "Phone__c";
+    PHONE = "Phone_Number__c";
     EMAIL = "Email__c";
-    PROPERTY = "Property";
-    AVAILABLE_DATE = "Available_Date_c";
+    PROPERTY = "Property__c";
+    AVAILABLE_DATE = "Available_Date__c";
 
+    /*
     handleSuccess(event) {
         const evt = new ShowToastEvent({
             title: 'Tour Created!',
@@ -24,13 +31,27 @@ export default class TourRequests extends LightningElement {
         });
         this.dispatchEvent(evt);
     }
+    */
 
     onSubmitHandler(event) {
      event.preventDefault();
     const fields = event.detail.fields;
     this.template
     .querySelector('.lightform').submit(fields);
-    this.handleSuccess
+    this.handleSuccess;
+    this.showMsg = true;
+
+    setTimeout(() => {
+        this.closeModel();
+    }, this.autoCloseTime);
     }
-    
+
+    handleReset(event){
+        const inputFields = this.template.querySelectorAll('lightning-input-field');
+if(inputFields){ inputFields.forEach(field => {
+    field.requestFullscreen();
+})}
+    }
+
+
 }
