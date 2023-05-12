f1c285284d611f3a98360ebe57cb79083e3c743d
Taylor B <tinacooltu@gmail.com>
2023-05-12 10:00:59 -0700

    New commit to my branch

--- force-app/main/default/lwc/tourRequests/tourRequests.js
+++ force-app/main/default/lwc/tourRequests/tourRequests.js
@@ -1,12 +1,20 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import AVAILABLE_DATE from '@salesforce/schema/Tour_Request__c.Available_Date__c';
-import CONTACT from '@salesforce/schema/Tour_Request__c.Contact__c';
 import PROPERTY from '@salesforce/schema/Tour_Request__c.Property__c';
+import EMAIL from '@salesforce/schema/Tour_Request__c.Email__c';
+import PHONE from '@salesforce/schema/Tour_Request__c.Phone__c';
+import NAME from '@salesforce/schema/Tour_Request__c.Name__c';
 export default class TourRequests extends LightningElement {
 
     objectApiName = "Tour_Request__c";
-    fields = [AVAILABLE_DATE, CONTACT, PROPERTY];
+    fields = [NAME, EMAIL, PHONE, PROPERTY, AVAILABLE_DATE];
+
+    NAME = "Name__c";
+    PHONE = "Phone__c";
+    EMAIL = "Email__c";
+    PROPERTY = "Property";
+    AVAILABLE_DATE = "Available_Date_c";
 
     handleSuccess(event) {
         const evt = new ShowToastEvent({
@@ -16,4 +24,13 @@ export default class TourRequests extends LightningElement {
         });
         this.dispatchEvent(evt);
     }
+
+    onSubmitHandler(event) {
+     event.preventDefault();
+    const fields = event.detail.fields;
+    this.template
+    .querySelector('.lightform').submit(fields);
+    this.handleSuccess
+    }
+    
 }
