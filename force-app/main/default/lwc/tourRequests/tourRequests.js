dd84a367f2ce3a192c03fc18131c28b87a88e4d5
David Melech <david984@revature.net>
2023-05-12 15:23:31 -0500

    added fix to

--- force-app/main/default/lwc/tourRequests/tourRequests.js
+++ force-app/main/default/lwc/tourRequests/tourRequests.js
@@ -1,29 +1,46 @@
-import { LightningElement } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { LightningElement ,api,track} from 'lwc';
+import NAME from '@salesforce/schema/Tour_Request__c.Name__c';
 import AVAILABLE_DATE from '@salesforce/schema/Tour_Request__c.Available_Date__c';
 import PROPERTY from '@salesforce/schema/Tour_Request__c.Property__c';
-import UNIT from '@salesforce/schema/Tour_Request__c.Unit__c';
-import NAME from '@salesforce/schema/Tour_Request__c.Name__c';
+import EMAIL from '@salesforce/schema/Tour_Request__c.Email__c';
+import PHONE from '@salesforce/schema/Tour_Request__c.Phone_Number__c';
 export default class TourRequests extends LightningElement {
 
+    
+    @track showMsg = false;
+    autoCloseTime = 5000;
+    @api
+    recordid
     objectApiName = "Tour_Request__c";
-    fields = [NAME, PROPERTY, UNIT, AVAILABLE_DATE];
-
-    handleSuccess(event) {
-        const evt = new ShowToastEvent({
-            title: 'Tour Created!',
-            message: 'Record ID:',
-            variant: 'success',
-        });
-        this.dispatchEvent(evt);
-    }
+    NAME = "Name__c";
+    PHONE = "Phone_Number__c";
+    EMAIL = "Email__c";
+    PROPERTY = "Property__c";
+    AVAILABLE_DATE = "Available_Date__c";
 
     onSubmitHandler(event) {
         event.preventDefault();
     const fields = event.detail.fields;
     this.template.querySelector('.lightform').submit(fields);
-    this.handleSuccess
-
+    this.handleReset()
+    this.showMsg = true
+    setTimeout(() => {
+        this.showsuccess();;
+    }, this.autoCloseTime);
+        
     }
-    
+    handleReset(event) {
+        const inputFields = this.template.querySelectorAll(
+            'lightning-input-field'
+        );
+        if (inputFields) {
+            inputFields.forEach(field => {
+                field.reset();  
+            });
+        }
+     }
+     showsuccess(e){
+        this.showMsg = false
+     }
+ 
 }
