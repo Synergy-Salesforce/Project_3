729d4c8c5e862128596d0856a4c4daa191d4e95a
Christopher Boamah Mensah <christopher431@revature.net>
2023-05-12 12:05:16 -0400

    about to work on hamburger menu

--- force-app/main/default/lwc/mainContainer/mainContainer.js
+++ force-app/main/default/lwc/mainContainer/mainContainer.js
@@ -2,11 +2,21 @@ import { LightningElement, wire } from 'lwc';
 import { subscribe, MessageContext, APPLICATION_SCOPE } from 'lightning/messageService';
 import MainChannel from '@salesforce/messageChannel/mainChannel__c';
 import LOGO_URL from '@salesforce/resourceUrl/Logo';
+import IS_GUEST from '@salesforce/user/isGuest';
+import { NavigationMixin } from 'lightning/navigation';
 
 
 export default class MainContainer extends LightningElement {
-    updatePageVar;
-    homePage;
+    homePage = true;
+    locationPage;
+    applicationPage;
+    showAllLocations = false;
+
+    isLoggedIn = !IS_GUEST;
+
+    logoutUrl = window.location.origin + '/secur/logout.jsp';
+
+    recordId;
 
     subscription = null;
     logoUrl = LOGO_URL;
@@ -14,12 +24,23 @@ export default class MainContainer extends LightningElement {
     @wire(MessageContext)
     mContext;
 
-    updatePageMethod(updater){
-        // reset all page booleans to false
+    resetPageBools(){
         this.homePage = false;
+        this.locationPage = false;
+        this.applicationPage = false;
+    }
+
+    updatePageMethod(updater){
+        this.resetPageBools();
 
         //set new page boolean to true
         switch (updater){
+            case 'locationPage':
+                this.locationPage = true;
+                break;
+            case 'applicationPage':
+                this.applicationPage = true;
+                break;
             default:
                 this.homePage = true;
         }
@@ -30,7 +51,11 @@ export default class MainContainer extends LightningElement {
             this.subscription = subscribe(
                 this.mContext,
                 MainChannel,
-                (payload) => {this.updatePageVar = payload.updatePage},
+                (payload) => {
+                    console.log(payload.recordId);
+                    this.recordId = payload.recordId;
+                    this.updatePageMethod(payload.updatePage);
+                    },
                 { scope : APPLICATION_SCOPE}
             );    
         }
@@ -38,6 +63,14 @@ export default class MainContainer extends LightningElement {
 
     connectedCallback(){
         this.subscribeToMessageChannel();
-        this.updatePageMethod();
+    }
+
+    handleLocationsClick(e){
+        this.showAllLocations = true;
+        this.updatePageMethod('homePage');
+    }
+
+    handleApplicationClick(e){
+        this.updatePageMethod('applicationPage');
     }
 }
