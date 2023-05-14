c81dbafa013ca3913b28011f234e3ec4aca1e9bf
David Melech <david984@revature.net>
2023-05-13 22:43:26 -0500

    household card add data list

--- force-app/main/default/lwc/householdMembersSub/householdMembersSub.js
+++ force-app/main/default/lwc/householdMembersSub/householdMembersSub.js
@@ -1,8 +1,24 @@
-import { LightningElement,api,track } from 'lwc';
-import NAME_FIELD from '@salesforce/schema/Household_People__c.Name';
-import TYPE from '@salesforce/schema/Household_People__c.Type__c';
-import RELATION from '@salesforce/schema/Household_People__c.Relation__c';
-import CONTACT from '@salesforce/schema/Household_People__c.Contact__c';
+import { LightningElement,api,track,wire } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/House_Member__c.Name';
+import TYPE from '@salesforce/schema/House_Member__c.Human_Type__c';
+import RELATION from '@salesforce/schema/House_Member__c.Relation__c';
+import CONTACT from '@salesforce/schema/House_Member__c.Contact__c';
+import RECORDTYPE from '@salesforce/schema/House_Member__c.RecordTypeId';
+import getHouseHoldMembersList from '@salesforce/apex/ProperHelper.getHouseHoldMembersList';
+import { refreshApex } from '@salesforce/apex';
+
+const actions = [
+    { label: 'Delete', name: 'delete' }
+];
+const columns = [
+   
+    { label: 'Name', fieldName: 'Name',fixedWidth: 100 },
+    { label: 'Type', fieldName: 'Human_Type__c',fixedWidth: 90 },
+    { label: 'Relation', fieldName: 'Relation__c',fixedWidth: 90 },
+    { type: 'action', typeAttributes: { rowActions: actions, menuAlignment: 'right' } },
+];
+
+
 
 export default class HouseholdMembersSub extends LightningElement {
     @api recordid;
@@ -11,20 +27,22 @@ export default class HouseholdMembersSub extends LightningElement {
     @api householddata;
     @api objectApiName;
     @track showMsg = false;
+    data = [];
     autoCloseTime = 5000;
     @api ConId 
-    
-    // Expose a field to make it available in the template
-    objectApiName = 'Household_People__c'
+    objectApiName ='House_Member__c'
+    error;
+    @track columns = columns;
     nameField = NAME_FIELD;
     typeField = TYPE;
     relationField = RELATION;
     
-   
+     @wire(getHouseHoldMembersList) householdlist;
     handleSubmit(event) {
         event.preventDefault();
     const fields = event.detail.fields;
     fields.Contact__c = this.recordid
+    fields.	RecordTypeId ='0128b000000dNzpAAE'
     this.template.querySelector('.lightform').submit(fields);
     this.handleReset()
     this.showMsg = true
@@ -37,6 +55,7 @@ export default class HouseholdMembersSub extends LightningElement {
         const inputFields = this.template.querySelectorAll(
             'lightning-input-field'
         );
+        refreshApex(this.householdlist);
         if (inputFields) {
             inputFields.forEach(field => {
                 field.reset();  
@@ -47,6 +66,19 @@ export default class HouseholdMembersSub extends LightningElement {
         this.showMsg = false
      }
 
+     handleRowAction(event) {
+        const action = event.detail.action;
+        const row = event.detail.row;
+        switch (action.name) {
+            case 'delete':
+                const rows = this.data;
+                const rowIndex = rows.indexOf(row);
+                rows.splice(rowIndex, 1);
+                this.data = rows;
+                break;
+                }
+
+        }
 }
 
     
