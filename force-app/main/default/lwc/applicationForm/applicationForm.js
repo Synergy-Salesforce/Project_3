c8211fe0f64913b0a28483c019b391e851ecf032
Sam Kuhn <kuhnsamuel2@gmail.com>
2023-05-16 15:08:26 -0500

    style looks good generally - manage household page incomplete

--- force-app/main/default/lwc/applicationForm/applicationForm.js
+++ force-app/main/default/lwc/applicationForm/applicationForm.js
@@ -3,37 +3,42 @@
   REVATURE LLC
 */
 import { LightningElement } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import APPLICATION_OBJECT from '@salesforce/schema/Application__c'; 
-import FIRSTNAME_FIELD from '@salesforce/schema/Application__c.First_Name__c';
-import LASTNAME_FIELD from '@salesforce/schema/Application__c.Last_Name__c';
-import MOBILE_FIELD from '@salesforce/schema/Application__c.Mobile__c';
-import EMAIL_FIELD from '@salesforce/schema/Application__c.Email__c';
-import MOVEINDATE_FIELD from '@salesforce/schema/Application__c.Move_In_Date__c';
-import PROPERTY_FIELD from '@salesforce/schema/Application__c.Property__c';
-import UNIT_FIELD from '@salesforce/schema/Application__c.Unit__c';
 
 export default class ApplicationForm extends LightningElement {
-  objectApiName = APPLICATION_OBJECT;
-  fields = [FIRSTNAME_FIELD, LASTNAME_FIELD, MOBILE_FIELD, EMAIL_FIELD, MOVEINDATE_FIELD, PROPERTY_FIELD, UNIT_FIELD];
+  objectApiName = "Application__c";
 
-  handleSuccess() {
-    const susccessToastEvent = new ShowToastEvent({
-      title: "Success",
-      message: "Your application was submitted",
-      variant: "success"
-    });
+  showMsg = false;
+  autoCloseTime = 5000;
 
-    this.dispatchEvent(susccessToastEvent);
-    const editForm = this.template.querySelector('lightning-record-form');
-    editForm.recordId = null; 
+  FIRSTNAME = "First_Name__c";
+  LASTNAME = "Last_Name__c";
+  MOBILE = "Mobile__c";
+  EMAIL = "Email__c";
+  MOVEINDATE = "Move_In_Date__c";
+  PROPERTY = "Property__c";
+  UNIT = "Unit__c";
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const fields = event.detail.fields;
+    this.template.querySelector('.light-form').submit(fields);
+    this.handleReset()
+    this.showMsg = true
+    setTimeout(() => {
+      this.showsuccess();
+    }, this.autoCloseTime);
   }
 
-  handleCancel() {
-    const cancelToastEvent = new ShowToastEvent({
-      title: "Failure",
-      message: "Your application was not submitted. Please contact support.",
-      variant: "error"
-    });
+  handleReset() {
+    const inputFields = this.template.querySelectorAll('lightning-input-field');
+    if (inputFields) {
+      inputFields.forEach(field => {
+        field.reset();  
+      });
+    }
+  }
+
+  showsuccess() {
+    this.showMsg = false;
   }
 }
