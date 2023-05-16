4e470a920a464c8a13bb7b9c264e74d95b2f2f48
Sam Kuhn <kuhnsamuel2@gmail.com>
2023-05-16 09:31:58 -0500

    functional fetch lease URL

--- force-app/main/default/lwc/lease/lease.js
+++ force-app/main/default/lwc/lease/lease.js
@@ -1,11 +1,42 @@
 import { LightningElement, wire, api } from 'lwc';
 import getConId from '@salesforce/apex/ProperHelper.getConId';
+import getLeaseFromContact from '@salesforce/apex/ProperHelper.getLeaseFromContact2';
+import USER_ID from '@salesforce/user/Id';
+
 
 export default class Lease extends LightningElement {
-    @api
-    userId;
+    // @api
+    // userId;
+    userId = USER_ID;
+    // @wire(getConId, {userId : '$userId'})
+    // wiredConId({data, error}){
+    //     if(data){
+    //         this.contactId = data;
+    //     }
+    // }
 
-    //get the contactId
-    @wire(getConId, {userId : '$userId'})
-    contactId;
+    @wire(getLeaseFromContact, {userId : '$userId'})
+    leaseUrl;
+
+    showLeaseUrl(){
+        console.log('Lease Url: ');
+        console.log(this.leaseUrl);
+    }
+
+    // connectedCallback(){
+    //     getConId({userId : this.userId})
+    //         .then((resultContactId) => {
+    //             console.log('Reached get Con ID');
+    //             console.log(resultContactId);
+    //             getLeaseFromContact({contactId : resultContactId})
+    //                 .then(resultLeaseUrl => {
+    //                     console.log('Reached get get Lease from Contact');
+    //                     this.leaseUrl = resultLeaseUrl;
+    //                 })
+    //                 .catch((error) => {
+    //                     console.log('Get Lease From Contact Error:');
+    //                     console.log(error);
+    //                 })
+    //         })
+    // }
 }
